752e5b1a8f81055ed160bc4b76ffc713b424530d
dweintrop <dnw882@dweintrop2-SESP.local>
2015-06-14 15:45:00 -0500

    updated droplet library

--- content/src/view.js
+++ content/src/view.js
@@ -2925,7 +2925,7 @@ function getPaneEditorFloatingBlocks(pane) {
   paneState.dropletEditor.floatingBlocks;
   var floatingBlocksString = '';
   paneState.dropletEditor.floatingBlocks.map(function(block){
-    floatingBlocksString += block.block.stringify() + '|';
+    floatingBlocksString += block.block.stringify({empty : "``", emptyIndent : "``"}) + '|';
   });
   return floatingBlocksString;
 }
